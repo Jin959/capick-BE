e6c3a0f11f5a6b98327f681aec630636650c623a
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-11-15 14:33:06 +0900

    [#1] chore: Spring 환경 및 의존성 설정
    - 프로젝트명 : capick
    - Language : Java 11
    - Build Tool : Gradle
    - 프레임워크 : Spring Boot 2.7.17
    - Spring Dependency : Web, Data JPA, Validation
    - 테스트 : Junit5, Mockito
    - DB : H2, MySQL 8
    - API 문서 : Swagger SpringDoc
    - 기타 라이브러리 : Lombok

---
+++ src/main/java/com/capick/capick/CapickApplication.java
@@ -0,0 +1,13 @@
+package com.capick.capick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapickApplication.class, args);
+    }
+
+}
