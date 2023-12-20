627e6619bc644f969da304be997a324f40e963c4
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-12-20 19:25:28 +0900

    [CP-4] feat: 회원 레포지토리 생성

---
+++ src/main/java/com/capick/capick/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.capick.capick.repository;
+
+import com.capick.capick.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
