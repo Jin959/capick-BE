7cab22a41145eda3c3de36dc41bf0d611674d0bb
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-12-20 19:47:17 +0900

    [CP-3] feat: 회원 컨트롤러 생성 및 회원가입 엔드포인트 개발

---
+++ src/main/java/com/capick/capick/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.capick.capick.controller;
+
+import com.capick.capick.dto.ApiResponse;
+import com.capick.capick.dto.request.MemberCreateRequest;
+import com.capick.capick.dto.response.MemberCreateResponse;
+import com.capick.capick.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/new")
+    public ApiResponse<MemberCreateResponse> createMember(@RequestBody MemberCreateRequest memberCreateRequest) {
+        return ApiResponse.ok(memberService.createMember(memberCreateRequest));
+    }
+
+}
