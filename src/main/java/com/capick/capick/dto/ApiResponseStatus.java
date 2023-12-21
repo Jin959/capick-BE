27c55d680932e0dbcfce5f31b1bd47e13fbf4870
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-12-21 20:15:04 +0900

    [CP-3] feat: 이메일과 닉네임 중복검사를 회원가입 서비스 로직에 추가

--- src/main/java/com/capick/capick/dto/ApiResponseStatus.java
+++ src/main/java/com/capick/capick/dto/ApiResponseStatus.java
@@ -12,6 +12,8 @@ public enum ApiResponseStatus {
 
     // 400
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 계정의 이메일 입니다."),
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 사용 중인 닉네임 입니다."),
 
     // 500
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 연결에 실패하였습니다.");
--- src/main/java/com/capick/capick/service/MemberService.java
+++ src/main/java/com/capick/capick/service/MemberService.java
@@ -3,10 +3,14 @@ package com.capick.capick.service;
 import com.capick.capick.domain.member.Member;
 import com.capick.capick.dto.request.MemberCreateRequest;
 import com.capick.capick.dto.response.MemberCreateResponse;
+import com.capick.capick.exception.BaseException;
 import com.capick.capick.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.capick.capick.domain.common.BaseStatus.*;
+import static com.capick.capick.dto.ApiResponseStatus.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,6 +21,14 @@ public class MemberService {
 
     @Transactional
     public MemberCreateResponse createMember(MemberCreateRequest request) {
+        if (memberRepository.existsByEmailAndStatus(request.getEmail(), ACTIVE)) {
+            throw BaseException.of(DUPLICATE_EMAIL);
+        }
+
+        if (memberRepository.existsByNicknameAndStatus(request.getNickname(), ACTIVE)) {
+            throw BaseException.of(DUPLICATE_NICKNAME);
+        }
+
         Member member = memberRepository.save(request.toEntity());
         return MemberCreateResponse.of(member);
     }
