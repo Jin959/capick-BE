09255c372e7054f280f000f05e702be5c8f637bd
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-12-21 19:47:45 +0900

    [CP-3] feat: 회원가입 시 이메일과 닉네임 중복 검사를 위한 레포지토리 개발

--- src/main/java/com/capick/capick/repository/MemberRepository.java
+++ src/main/java/com/capick/capick/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.capick.capick.repository;
 
+import com.capick.capick.domain.common.BaseStatus;
 import com.capick.capick.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmailAndStatus(String email, BaseStatus status);
+
+    boolean existsByNicknameAndStatus(String nickname, BaseStatus status);
+
 }
