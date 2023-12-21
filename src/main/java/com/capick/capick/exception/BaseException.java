68542c36b5743c5ef8d8d768ab55378b20180f68
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-12-21 20:02:54 +0900

    [CP-4] refactor: 공통 예외 생성자의 접근 제한자를 private 으로 설정

--- src/main/java/com/capick/capick/exception/BaseException.java
+++ src/main/java/com/capick/capick/exception/BaseException.java
@@ -8,7 +8,7 @@ public class BaseException extends RuntimeException {
 
     private ApiResponseStatus status;
 
-    public BaseException(ApiResponseStatus status) {
+    private BaseException(ApiResponseStatus status) {
         super(status.getMessage());
         this.status = status;
     }
