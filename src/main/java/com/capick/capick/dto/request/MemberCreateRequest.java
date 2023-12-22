5c5f2b6dc71028ead4913a37945d57fdbd2db61f
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-12-22 18:12:38 +0900

    [CP-3] config: 원할한 컨트롤러 계층 WebMvcTest 를 위한 JpaAuditing 등의 설정

--- src/main/java/com/capick/capick/dto/request/MemberCreateRequest.java
+++ src/main/java/com/capick/capick/dto/request/MemberCreateRequest.java
@@ -3,8 +3,10 @@ package com.capick.capick.dto.request;
 import com.capick.capick.domain.member.Member;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class MemberCreateRequest {
 
     private String email;
