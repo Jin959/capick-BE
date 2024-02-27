6a79f9322a6ae3f64b0da2d3ec22578e459c3993
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-02-27 18:19:36 +0900

    [CP-23] feat: 비밀번호 수정 요청 DTO 에 비밀번호 확인 추가

--- src/main/java/com/capick/capick/dto/request/MemberPasswordRequest.java
+++ src/main/java/com/capick/capick/dto/request/MemberPasswordRequest.java
@@ -15,17 +15,25 @@ public class MemberPasswordRequest {
     @NotNull(message = "리소스 아이디를 입력해주세요.")
     private Long id;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "기존 비밀번호를 입력해주세요.")
     @Pattern(
             regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()?])(?=\\S+$)[0-9a-zA-Z!@#$%^&*()?]{8,20}$",
-            message = "비밀번호는 띄어쓰기 없는 영문/숫자/특수문자(!@#$%^&*()?)를 조합하여 8자~20자리로 작성해주세요."
+            message = "기존 비밀번호는 띄어쓰기 없는 영문/숫자/특수문자(!@#$%^&*()?)를 조합하여 8자~20자리로 작성해주세요."
     )
     private String password;
 
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()?])(?=\\S+$)[0-9a-zA-Z!@#$%^&*()?]{8,20}$",
+            message = "새 비밀번호는 띄어쓰기 없는 영문/숫자/특수문자(!@#$%^&*()?)를 조합하여 8자~20자리로 작성해주세요."
+    )
+    private String newPassword;
+
     @Builder
-    public MemberPasswordRequest(Long id, String password) {
+    public MemberPasswordRequest(Long id, String password, String newPassword) {
         this.id = id;
         this.password = password;
+        this.newPassword = newPassword;
     }
 
 }
