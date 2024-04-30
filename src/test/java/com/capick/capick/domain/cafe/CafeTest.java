ad94ffcb889a68e6689e721e7f945111e4dcecde
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-04-30 20:46:44 +0900

    [CP-29] test: 까페 타입 갱신 도메인 로직 테스트 작성

---
+++ src/test/java/com/capick/capick/domain/cafe/CafeTest.java
@@ -0,0 +1,79 @@
+package com.capick.capick.domain.cafe;
+
+import com.capick.capick.domain.review.Review;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CafeTest {
+
+    @Test
+    @DisplayName("성공: 까페 타입 갱신 시 리뷰에서 매겨진 지수들이 누적되어 더해진다.")
+    void updateIndexes() {
+        // given
+        Review review = createReview("일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 3, 3, 4, 3);
+        Cafe cafe = createCafe("스타벅스 광화문점", "1234567", "https://place.url");
+
+        // when
+        cafe.updateCafeType(review);
+
+        // then
+        assertThat(cafe)
+                .extracting("coffeeIndex", "spaceIndex", "priceIndex", "noiseIndex")
+                .contains(3, 3, 4, 3);
+
+    }
+
+    @Test
+    @DisplayName("성공: 까페 타입 갱신 시 누적된 지수들 중 가장 큰 값으로 까페의 타입이 정해진다.")
+    void updateCafeType() {
+        // given
+        Review review = createReview("일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 3, 3, 4, 3);
+        Cafe cafe = createCafe("스타벅스 광화문점", "1234567", "https://place.url");
+
+        // when
+        cafe.updateCafeType(review);
+
+        // then
+        assertThat(cafe.getCafeType()).isEqualByComparingTo(CafeType.COST_EFFECTIVE);
+
+    }
+
+    @Test
+    @DisplayName("경계: 까페 타입 갱신 시 누적된 지수 중 최대값이 없으면 NONE 으로 기록한다.")
+    void updateCafeTypeNone() {
+        // given
+        Review review = createReview("일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 3, 3, 3, 3);
+        Cafe cafe = createCafe("스타벅스 광화문점", "1234567", "https://place.url");
+
+        // when
+        cafe.updateCafeType(review);
+
+        // then
+        assertThat(cafe.getCafeType()).isEqualByComparingTo(CafeType.NONE);
+
+    }
+
+    private static Review createReview(String visitPurpose, String content, String menu,
+                                       int coffeeIndex, int spaceIndex, int priceIndex, int noiseIndex) {
+        return Review.builder()
+                .visitPurpose(visitPurpose)
+                .content(content)
+                .menu(menu)
+                .coffeeIndex(coffeeIndex)
+                .spaceIndex(spaceIndex)
+                .priceIndex(priceIndex)
+                .noiseIndex(noiseIndex)
+                .build();
+    }
+
+    private Cafe createCafe(String name, String kakaoPlaceId, String kakaoDetailPageUrl) {
+        return Cafe.builder()
+                .name(name)
+                .kakaoPlaceId(kakaoPlaceId)
+                .kakaoDetailPageUrl(kakaoDetailPageUrl)
+                .build();
+    }
+
+}
