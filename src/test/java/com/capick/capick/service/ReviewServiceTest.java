f13acf701d14549ded67ed722c1e80a946867058
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-05-22 22:00:39 +0900

    [CP-29] test: 리뷰 생성 기능 서비스 계층 예외 케이스 작성

--- src/test/java/com/capick/capick/service/ReviewServiceTest.java
+++ src/test/java/com/capick/capick/service/ReviewServiceTest.java
@@ -7,6 +7,9 @@ import com.capick.capick.dto.request.CafeCreateRequest;
 import com.capick.capick.dto.request.LocationCreateRequest;
 import com.capick.capick.dto.request.ReviewCreateRequest;
 import com.capick.capick.dto.response.ReviewResponse;
+import com.capick.capick.exception.DomainLogicalException;
+import com.capick.capick.exception.DomainPoliticalArgumentException;
+import com.capick.capick.exception.NotFoundResourceException;
 import com.capick.capick.repository.CafeRepository;
 import com.capick.capick.repository.MemberRepository;
 import com.capick.capick.repository.ReviewRepository;
@@ -22,8 +25,7 @@ import java.util.List;
 
 import static com.capick.capick.domain.cafe.CafeTheme.NORMAL;
 import static com.capick.capick.domain.cafe.CafeType.COST_EFFECTIVE;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
+import static org.assertj.core.api.Assertions.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -130,6 +132,77 @@ class ReviewServiceTest {
                 .contains(
                         tuple(COST_EFFECTIVE, NORMAL)
                 );
+
+    }
+
+    @Test
+    @DisplayName("예외: 리뷰를 생성하는 회원이 탈퇴 처리 되었거나 존재하지 않는 회원이면 예외가 발생한다.")
+    void createReviewNotExistMember() {
+        // given
+        Long notExistWriterId = 1L;
+
+        Location cafeLocation = createLocation(37.57122962143047, 126.97629649901215, "서울 종로구 세종로 00-0", "서울 종로구 세종대로 000");
+        Cafe cafe = createCafe("스타벅스 광화문점", "1234567", "https://place.url", cafeLocation);
+        cafeRepository.save(cafe);
+
+        CafeCreateRequest cafeCreateRequest
+                = createCafeCreateRequest("스타벅스 광화문점", "1234567", "https://place.url");
+        ReviewCreateRequest reviewCreateRequest
+                = createReviewCreateRequest(notExistWriterId, cafeCreateRequest, "일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 3, 3, 3, 3, "normal");
+        
+        // when // then
+        assertThatThrownBy(() -> reviewService.createReview(reviewCreateRequest))
+                .isInstanceOf(NotFoundResourceException.class)
+                .hasMessage("존재하지 않는 회원입니다.");
+
+    }
+
+    @Test
+    @DisplayName("예외: 등록된 까페에만 리뷰를 작성할 수 있다. 첫 리뷰 생성 시 까페가 등록된 적이 없어 등록하려는데 까페의 위치 정보를 받지 않으면 예외가 발생한다.")
+    void createFirstReviewWithoutLocation() {
+        // given
+        Member writer = createMember("email01@naver.com", "password01%^&", "nickname01");
+        memberRepository.save(writer);
+        Long writerId = writer.getId();
+
+        CafeCreateRequest cafeCreateRequest
+                = createCafeCreateRequest("스타벅스 광화문점", "1234567", "https://place.url");
+        ReviewCreateRequest reviewCreateRequest
+                = createReviewCreateRequest(writerId, cafeCreateRequest, "일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 3, 3, 4, 3, "normal");
+
+        // when // then
+        assertThatThrownBy(() -> reviewService.createReview(reviewCreateRequest))
+                .isInstanceOf(DomainLogicalException.class)
+                .hasMessage("까페에 첫 리뷰를 등록할 때는 까페의 위치 정보가 필요합니다.");
+
+    }
+
+    @Test
+    @DisplayName("예외: 리뷰 작성 시 간접적인 설문을 통해 까페의 타입 지수를 1 에서 5 로 매길 수 있다. 1 부터 5 이외의 타입 지수를 입력받으면 예외가 발생한다.")
+    void createReviewWithCafeTypeIndexOutOfRange() {
+        // given
+        Member writer = createMember("email01@naver.com", "password01%^&", "nickname01");
+        memberRepository.save(writer);
+        Long writerId = writer.getId();
+
+        Location cafeLocation = createLocation(37.57122962143047, 126.97629649901215, "서울 종로구 세종로 00-0", "서울 종로구 세종대로 000");
+        Cafe cafe = createCafe("스타벅스 광화문점", "1234567", "https://place.url", cafeLocation);
+        cafeRepository.save(cafe);
+
+        CafeCreateRequest cafeCreateRequest
+                = createCafeCreateRequest("스타벅스 광화문점", "1234567", "https://place.url");
+        ReviewCreateRequest reviewCreateRequestMinus
+                = createReviewCreateRequest(writerId, cafeCreateRequest, "일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 0, 3, 3, 3, "normal");
+        ReviewCreateRequest reviewCreateRequestOver
+                = createReviewCreateRequest(writerId, cafeCreateRequest, "일하거나 책읽기 좋아요", "리뷰 내용", "아메리카노", 3, 6, 3, 3, "normal");
+
+        // when // then
+        assertThatThrownBy(() -> reviewService.createReview(reviewCreateRequestMinus))
+                .isInstanceOf(DomainPoliticalArgumentException.class)
+                .hasMessage("리뷰 작성 시 까페 타입 지수는 1 부터 5 여야 합니다.");
+        assertThatThrownBy(() -> reviewService.createReview(reviewCreateRequestOver))
+                .isInstanceOf(DomainPoliticalArgumentException.class)
+                .hasMessage("리뷰 작성 시 까페 타입 지수는 1 부터 5 여야 합니다.");
 
     }
 
