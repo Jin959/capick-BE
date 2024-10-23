160126c87f7b62be1005204173a581525523c50a
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-10-24 02:08:32 +0900

    [CP-34] feat: 외부 저장소 고아 파일 기록 리스트 조회 레파지토리 개발

--- src/main/java/com/capick/capick/repository/history/storage/StorageOrphanFileHistoryRepository.java
+++ src/main/java/com/capick/capick/repository/history/storage/StorageOrphanFileHistoryRepository.java
@@ -4,6 +4,11 @@ import com.capick.capick.domain.history.storage.StorageOrphanFileHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StorageOrphanFileHistoryRepository extends JpaRepository<StorageOrphanFileHistory, Long> {
+
+    boolean existsByUrlInAndIsHardDeleted(List<String> urls, boolean isHardDeleted);
+
 }
