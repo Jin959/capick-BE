5037b7dde0700fb311fa0d4f6f544317d6759b61
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2024-12-12 02:14:30 +0900

    [CP-49] feat: 카페별 리뷰 페이지네이션 조회 기능 서비스 계층 개발

--- src/main/java/com/capick/capick/service/CafeService.java
+++ src/main/java/com/capick/capick/service/CafeService.java
@@ -1,15 +1,24 @@
 package com.capick.capick.service;
 
 import com.capick.capick.domain.cafe.Cafe;
+import com.capick.capick.domain.review.Review;
+import com.capick.capick.domain.review.ReviewImage;
 import com.capick.capick.dto.PageResponse;
 import com.capick.capick.dto.response.CafeResponse;
 import com.capick.capick.dto.response.ReviewSimpleResponse;
 import com.capick.capick.exception.NotFoundResourceException;
 import com.capick.capick.repository.CafeRepository;
+import com.capick.capick.repository.ReviewImageRepository;
+import com.capick.capick.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.capick.capick.domain.common.BaseStatus.ACTIVE;
 import static com.capick.capick.dto.ApiResponseStatus.NOT_FOUND_CAFE;
@@ -21,6 +30,10 @@ public class CafeService {
 
     private final CafeRepository cafeRepository;
 
+    private final ReviewRepository reviewRepository;
+
+    private final ReviewImageRepository reviewImageRepository;
+
     public CafeResponse getCafeByMapVendorPlaceId(String placeId) {
         Cafe cafe = cafeRepository.findByKakaoPlaceIdAndStatus(placeId, ACTIVE)
                 .orElseThrow(() -> NotFoundResourceException.of(NOT_FOUND_CAFE));
@@ -28,6 +41,18 @@ public class CafeService {
     }
 
     public PageResponse<ReviewSimpleResponse> getReviewsByCafeWithMapVendorPlaceId(String placeId, Pageable pageable) {
-        return null;
+        Cafe cafe = cafeRepository.findByKakaoPlaceIdAndStatus(placeId, ACTIVE)
+                .orElseThrow(() -> NotFoundResourceException.of(NOT_FOUND_CAFE));
+
+        Page<Review> reviewPage = reviewRepository.findPageByCafeAndStatus(cafe, ACTIVE, pageable);
+
+        List<ReviewImage> thumbnails = reviewImageRepository.findReviewThumbnailsBy(reviewPage.getContent(), ACTIVE);
+        Map<Long, ReviewImage> thumbnailMap = thumbnails.stream()
+                .collect(Collectors.toMap(thumbnail -> thumbnail.getReview().getId(), thumbnail -> thumbnail));
+
+        return PageResponse.of(reviewPage.map(review ->
+                ReviewSimpleResponse.of(review, thumbnailMap.get(review.getId()), cafe)
+        ));
     }
+
 }
